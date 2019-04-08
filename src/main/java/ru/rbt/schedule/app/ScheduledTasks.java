0e447feabf4e5aa86e9e688d2d9bec97136cf77a
BritikovMI <BritikovMI@rbtechnologies.ru>
2019-04-08 19:12:02 +0300

    Cron

--- src/main/java/ru/rbt/schedule/app/ScheduledTasks.java
+++ src/main/java/ru/rbt/schedule/app/ScheduledTasks.java
@@ -19,7 +19,15 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @RequestMapping("/")
-    @Scheduled(fixedRate = 5000)
+//    @Scheduled(cron = "0 0 21 * * ?")// Каждый день в 21:00
+    @Scheduled(cron = "*/60 * * * * *") //Каждую минуту
+    //second, minute, hour, day of month, month, day(s) of week
+    //* "0 0 * * * *" = the top of every hour of every day.
+    //* "*/10 * * * * *" = every ten seconds.
+    //* "0 0 8-10 * * *" = 8, 9 and 10 o'clock of every day.
+    //* "0 0/30 8-10 * * *" = 8:00, 8:30, 9:00, 9:30 and 10 o'clock every day.
+    //* "0 0 9-17 * * MON-FRI" = on the hour nine-to-five weekdays
+    //* "0 0 0 25 12 ?" = every Christmas Day at midnight
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
